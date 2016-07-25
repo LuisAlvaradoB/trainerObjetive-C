b0aa6177fb47490efd576a635c87f99795fb5a99
LuisAlvaradoB <luis.alvarado@protonmail.com>
2016-07-25 12:40:04 -0400

    Conceptos Basicos en Objetive-C

---
+++ Ejemplo 1 Objetive-C/Ejemplo 1 Objetive-C/conceptosBasicos.m
@@ -0,0 +1,11 @@
+//
+//  conceptosBasicos.m
+//  Ejemplo 1 Objetive-C
+//
+//  Created by Luis Alvarado on 25-07-16.
+//  Copyright © 2016 LuisAlvarado. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+// Conceptos Básicos en objetive-C
---
+++ Ejemplo 1 Objetive-C/Ejemplo 1 Objetive-C/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Ejemplo 1 Objetive-C
+//
+//  Created by Luis Alvarado on 25-07-16.
+//  Copyright © 2016 LuisAlvarado. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
