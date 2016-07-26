d62e617c396fed688f3508dedebe95fdbc80f790
LuisAlvaradoB <luis.alvarado@protonmail.com>
2016-07-26 15:42:33 -0400

    Ejercicio de funciones  "Calculadora"

---
+++ Ejemplo 1/Ejemplo 1/main.m
@@ -0,0 +1,38 @@
+//
+//  main.m
+//  Ejemplo 1
+//
+//  Created by Luis Alvarado on 25-07-16.
+//  Copyright © 2016 LuisAlvarado. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int calcularCuadrado (int x) {
+
+    int resultado = x * x;
+    return resultado;
+}
+
+
+
+
+
+
+
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+       
+        
+        
+        
+        
+        
+        NSLog(@"El resultado es : %i", calcularCuadrado(2));
+        
+        
+        
+    }
+    return 0;
+}
