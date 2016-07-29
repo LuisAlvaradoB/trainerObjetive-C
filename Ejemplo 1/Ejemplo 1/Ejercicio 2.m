14f6701be86df664aa0ed73c3f3ad2e3e9f72d7d
LuisAlvaradoB <luis.alvarado@protonmail.com>
2016-07-29 10:22:35 -0400

    Ejercicio con condionales Objetive-C

---
+++ Ejemplo 1/Ejemplo 1/Ejercicio 2.m
@@ -0,0 +1,24 @@
+//
+//  Ejercicio 2.m
+//  Ejemplo 1
+//
+//  Created by Luis Alvarado on 29-07-16.
+//  Copyright © 2016 LuisAlvarado. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        int temperatura = 15;
+        if (temperatura >= 22) {
+            NSLog(@"Hace calor");
+        }else{
+            NSLog(@"Hace una temperatura agradable");
+        }
+        
+        
+        
+    }return 0;
+}
---
+++ Ejemplo 2/Ejemplo 2/Ejemplo 2/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Ejemplo 2
+//
+//  Created by Luis Alvarado on 29-07-16.
+//  Copyright © 2016 LuisAlvarado. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+      
+        int temperatura = 15;
+        
+        if (temperatura >= 22) {
+            NSLog(@"Hace calor");
+        }else {
+            NSLog(@"Hce una temperatura agradable");
+    }
+  }return 0;
+}
